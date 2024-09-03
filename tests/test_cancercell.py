f9f3d5ab969bf5fa2a6a98f9ee4589d9eea440c1
Alfredo Hernández <alfredo.hernandez@ug.uchile.cl>
2024-09-03 05:18:33 -0400

    modify test

--- tests/test_cancercell.py
+++ tests/test_cancercell.py
@@ -1,1 +1,17 @@
-import metaspread.cancercell
+import metaspread.cancermodel
+import pytest
+#todo: model is not callable (duh! I think I cannot call a private variable (is it though?))
+#todo: use tmp_path_facorty to create the model once, and use it for the rest of the tests
+
+def test_phenotype(tmp_path) -> None:
+    temp_simulation_folder = tmp_path / "test_simulation"
+    temp_simulation_folder.mkdir()
+    model = metaspread.cancermodel(
+        number_of_initial_cells=30,
+        width=201,
+        height=201,
+        grids_number=2,
+        max_steps=1000,
+        data_collection_period=10,
+        new_simulation_folder=temp_simulation_folder)
+    assert model.data_collection_period == 10;
